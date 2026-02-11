75b00ebafc7884f17a901bac7e01866a087e7feb
chaiawat <6714951003@rbru.ac.th>
2026-02-11 11:44:39 +0700

    Test CD auto deploy

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from textblob import TextBlob
 
 st.set_page_config(page_title="My AI Lab", page_icon=":brain:")
 
-st.title(":brain: AI for Sentiment Analysis")
+st.title("AI for Sentiment Analysis - CD TEST SUCCESS")
+st.markdown("### 🚀 This version deployed automatically via CD")
 st.write("Enter an English message in the box below, and AI will determine whether it's Positive or Negative ")
 
 text_input = st.text_area("Input Text (English only):", height=150)
